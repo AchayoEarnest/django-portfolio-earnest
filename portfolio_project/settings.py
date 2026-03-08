c979fc48f86c829c973f1b194aa2ea051c1f2e94
Earnest Achayo <earnytechlive@gmail.com>
2026-03-08 15:15:06 +0300

    updated vercel specific setting

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -4,8 +4,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-earnest-portfolio-2024-secure-key'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = ['django-portfolio-earnest.vercel.app', 'localhost', '127.0.0.1']
+
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
@@ -58,9 +62,16 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATIC_DIRS = [os.path.join(BASE_DIR, 'accounts/static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = False
+    SECURE_HSTS_SECONDS = 0
